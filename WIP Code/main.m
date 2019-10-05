3b60cb534acced777da9d56ab7d79dbd51d82a92
Boikanyo13 <bv.radiokana@gmail.com>
2019-10-05 23:20:44 +0200

    Calculated BER & SNR, also plotted the BER vs SNR graph

--- WIP Code/main.m
+++ WIP Code/main.m
@@ -12,44 +12,102 @@ close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%TRANSMITER%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Generate random binary message
 
-numWords = 1;   %number of words
-M = 16;         %Modulation order
-k = 4;          %message length (length of words)
-msgTx = GenerateMSG(k,M)
-
-
-% BCH encoding
-
+% numWords = 1;   %number of words
+% M = 16;         %Modulation order
+% k = 4;          %message length (length of words)
+% msgTx = GenerateMSG(k,M)
+% 
+% 
+% % BCH encoding
+% 
+% m = 3;
+% n = 2^(m)-1;    %codeword length
+% r = k/n;        %code rate
+% [t, encodedMSG] = BCHEncoder_(msgTx, n,k);
+% 
+% 
+% %M-QAM Modulation
+% 
+% [modulatedMSG] = M_QAM(encodedMSG, M);
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%RayleighChannel%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+messageLength = 10000;     %number of words
+EbNo = 0:30;                %ratio of bit energy to noise power spectral density
+M = 16;                      %Modulation order
+k = 4;                       %message length (length of words)
 m = 3;
-n = 2^(m)-1;    %codeword length
-r = k/n;        %code rate
-[t, encodedMSG] = BCHEncoder_(msgTx, n,k);
+n = 2^(m)-1;                 %codeword length
+r = k/n;                     %code rate
+
+errorRate = comm.ErrorRate;
+errorStats = zeros(3,1);
+BER = zeros(1,length(EbNo));
 
 
-%M-QAM Modulation
+for i = 1:length(EbNo)
+    
+    SNR = EbNo(i) + 10*log10(r) + 10*log10(log2(M));      %signal to noise ratio
+    errorStats = zeros(3,1);
+    
+    while errorStats(3) < messageLength
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%TRANSMITER%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Generate random binary message
+        
+        msgTx = GenerateMSG(k,M);
+             
+        % BCH encoding
+        
+        [t, encodedMSG] = BCHEncoder_(msgTx, n,k);
+              
+        %M-QAM Modulation
+        
+        [modulatedMSG] = M_QAM(encodedMSG, M);
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%RayleighChannel%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        %Introduce AWGN -Noise
+        [noisyMSG] = rayleighChannel(modulatedMSG, SNR);
+        
+        % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%RECEIVER%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        %M-QAM Demodulation
+        
+        [demodulatedMSG] = M_QAM_Demodulator(noisyMSG, M);
+        
+        %Decode message
+        
+        msgRx= BCHDecoder(demodulatedMSG, n,k);
+        
+        %Confirm if codeword is decoded correctly
+        
+%         isCorrect = isequal(msgTx, msgTx);
+        errorStats = errorRate(msgRx,msgTx );
+        
+    end
+    
+    %Generate error stats to compare the transmitted msg to the received msgs
+        %The ErrorRate object returns 3 values
+        %1--> bit error rate
+        %2-->total error count
+        %3-->total number of bits
+        
+        
+        BER(i) = errorStats(1);
+        
+        reset(errorRate);
+end
 
-[modulatedMSG] = M_QAM(encodedMSG, M);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%RayleighChannel%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+semilogy(EbNo,BER, 'bp-')
 
-EbNo = 10;              %ratio of bit energy to noise power spectral density
-SNR = EbNo + 10*log10(r) + 10*log10(log2(M));
+s = scatterplot(modulatedMSG,1,0,'r*'); 
+hold on;
+scatterplot(noisyMSG,1,0,'bp',s)
+title('M-QAM Constellation');
+grid on;
 
-%Introduce AWGN -Noise
-[noisyMSG] = rayleighChannel(modulatedMSG, SNR )
 
-% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%RECEIVER%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%M-QAM Demodulation
-
-[demodulatedMSG] = M_QAM_Demodulator(noisyMSG, M);
-
-%Decode message
-
-msgRx= BCHDecoder(demodulatedMSG, n,k);
-
-%Confirm if codeword is decoded correctly
-
-isCorrect = isequal(msgTx, msgTx)
 
 
--- WIP Code/rayleighChannel.m
+++ WIP Code/rayleighChannel.m
@@ -8,12 +8,12 @@ function [noisySIG] = rayleighChannel(modulatedMSG, SNR )
 noisySIG = awgn(modulatedMSG, SNR);
 %Plot constellation
 
-scatterplot(noisySIG,1,0,'b*');         
-title('M-QAM Constellation- AWGN');
-grid on;
-ax = gca;                        % gets the current axes
-ax.XAxisLocation = 'origin';     % sets them to zero
-ax.YAxisLocation = 'origin';     % sets them to zero
+% scatterplot(noisySIG,1,0,'b*');         
+% title('M-QAM Constellation- AWGN');
+% grid on;
+% ax = gca;                        % gets the current axes
+% ax.XAxisLocation = 'origin';     % sets them to zero
+% ax.YAxisLocation = 'origin';     % sets them to zero
 
 
 end
