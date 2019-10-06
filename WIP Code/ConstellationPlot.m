c684339c29992e0f245e8a7a8b3bdfe82205fff7
Boikanyo13 <bv.radiokana@gmail.com>
2019-10-06 02:27:32 +0200

    Minor changes

--- WIP Code/ConstellationPlot.m
+++ WIP Code/ConstellationPlot.m
@@ -3,13 +3,13 @@ function [] = ConstellationPlot(noisyComponents,modulatedTx)
 %This function plots the constellations for different M-QAM modulation
 %schemes
 
-% figure;
+
 constellaton = scatterplot(noisyComponents,1,0,'k.');
 title('BCH(n,k) M-QAM Constellation');
 grid on
 hold on;
 scatterplot(modulatedTx,1,0,'rp',constellaton);
 
-hold off
+
 
 end
